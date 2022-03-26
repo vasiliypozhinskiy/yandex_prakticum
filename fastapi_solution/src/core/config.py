6dd90e539d5571c0e2c954656221c3faf68a0f20
Vlad <Promotion-kmv@yandex.ru>
2022-03-26 21:49:33 +0300

    добавил кэш в фильмах

--- fastapi_solution/src/core/config.py
+++ fastapi_solution/src/core/config.py
@@ -11,10 +11,13 @@ PROJECT_NAME = os.getenv('PROJECT_NAME', 'movies')
 # TODO: Добавить настройки для redis
 
 # Настройки Redis
-# REDIS_HOST = os.getenv('REDIS_HOST', '127.0.0.1')
-# REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
+REDIS_HOST = os.getenv('REDIS_HOST', '127.0.0.1')
+REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
 
 ELASTIC_HOST = os.getenv('ELASTIC_HOST', '127.0.0.1')
 ELASTIC_PORT = int(os.getenv('ELASTIC_PORT', 9200))
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Время работы кэша
+FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5  # 5 минут
--- fastapi_solution/src/services/utils.py
+++ fastapi_solution/src/services/utils.py
@@ -1,5 +1,5 @@
 from typing import Optional, List
-
+import hashlib
 from pydantic import parse_obj_as
 
 from services.mixin import Schemas
@@ -8,34 +8,48 @@ from services.mixin import Schemas
 def get_params_films_to_elastic(
         page_size: int = 10, genre: str = None, query: str = None
 ) -> dict:
+
     films_search = None
+    print(genre, query)
     if genre:
         films_search = {"fuzzy": {"genre": {"value": genre}}}
-    if query and genre:
 
+    if query and genre:
+        print('query')
         body: dict = {
             "size": page_size,
             "query": {
                 "bool": {
-                    "must": {"match": {"title": {"query": query,
-                                                 "fuzziness": "auto"}}},
+                    "must": {"match": {"title": {"query": query, "fuzziness": "auto"}}},
+                }
+            },
+        }
+    elif query and not genre:
+        body: dict = {
+            "size": page_size,
+            "query": {
+                "bool": {
+                    "must": {"match": {"title": {"query": query, "fuzziness": "auto"}}},
+                }
+            },
+        }
+    elif not query and not genre:
+        body: dict = {
+            "size": page_size,
+        }
+
+    else:
+        print('else')
+        body: dict = {
+            "size": page_size,
+            "query": {
+                "bool": {
+                    "must": {
+                        "match_all": {},
+                    },
                     "filter": films_search,
                 }
             },
-        }
-    elif query:
-        body: dict = {
-            "size": page_size,
-            "query": {
-                "bool": {
-                    "must": {"match": {"title": {"query": query,
-                                                 "fuzziness": "auto"}}},
-                }
-            },
-        }
-    else:
-        body: dict = {
-            "size": page_size,
         }
     return body
 
@@ -45,3 +59,9 @@ def get_hits(docs: Optional[dict], schema: Schemas):
     data: list = [row.get("_source") for row in hits]
     parse_data = parse_obj_as(List[schema], data)
     return parse_data
+
+def create_hash_key(index: str, params: str) -> str:
+    """Хешируем ключ по индексу и параметрам запроса """
+    hash_key = hashlib.md5(params.encode()).hexdigest()
+    return f"{index}:{hash_key}"
+
