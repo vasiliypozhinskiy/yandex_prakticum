9179f584387931338d45c42c6a8f7395b98f6b2d
Vlad <Promotion-kmv@yandex.ru>
2022-03-28 11:57:39 +0300

    delete FILM_CACHE_EXPIRE

--- fastapi_solution/src/services/mixin.py
+++ fastapi_solution/src/services/mixin.py
@@ -3,7 +3,6 @@ from typing import Optional, Union
 from aioredis import Redis
 from elasticsearch import AsyncElasticsearch
 
-from core.config import FILM_CACHE_EXPIRE_IN_SECONDS
 from models.film import Film
 from models.genre import Genre
 from models.person import Person
