bdc14d5b3cf85cb41fedd40afa22c44936aba5c2
Vlad <Promotion-kmv@yandex.ru>
2022-04-09 23:10:10 +0300

    add test person

--- fastapi_solution/tests/functional/src/test_movies.py
+++ fastapi_solution/tests/functional/src/test_movies.py
@@ -6,28 +6,24 @@ import ast
 
 from http import HTTPStatus
 
+from testdata.movies_data.film_search_params import film_list_params
+from testdata.movies_data.get_by_film_id import films_data, expected_film_data, expect_not_found_film_data
+from utils.hash_creater_for_redis import create_hash_key
 from utils.elastic_loader import ElasticLoader
 
-from testdata.movies_data.film_search_params import film_list_params
-from testdata.movies_data.get_by_id import films_data, expected_film_data, expect_not_found_film_data
-from utils.hash_creater_for_redis import create_hash_key
-
+check_list = ['id', 'title', 'imdb_rating', 'description', 'genre', 'actors', 'writers', 'director']
 
 @pytest.mark.usefixtures("create_movies_schema")
 class TestMovies:
     async def test_get_film_by_id(self, es_client, make_get_request, redis_client):
         response = await make_get_request(f"films/{expected_film_data['id']}")
         body = ast.literal_eval(response.data.decode('utf-8'))
-        data_list = ['id', 'title', 'imdb_rating', 'description', 'genre', 'actors', 'writers', 'director']
         assert HTTPStatus.OK == response.status
-
-        for i in data_list:
+        for i in check_list:
             assert expected_film_data.get(i) == body.get(i)
-
         assert redis_client.get(key=expected_film_data['id']) is not None
         await redis_client.flushall()
         assert await redis_client.get(key=expected_film_data['id']) is None
-
         # проверка несуществуеего фильма
         response = await make_get_request(f"films/{expect_not_found_film_data['id']}")
         assert HTTPStatus.NOT_FOUND == response.status
@@ -38,7 +34,7 @@ class TestMovies:
             *film_list_params
         ]
     )
-    async def test_get_search_film(self, es_client, make_get_request, redis_client, method, query, expected_status):
+    async def test_get_search_film(self, make_get_request, redis_client, method, query, expected_status):
         await asyncio.sleep(0.3)
         response = await make_get_request(method=f"{method}", params=query)
         body = ast.literal_eval(response.data.decode('utf-8'))
@@ -59,7 +55,6 @@ class TestMovies:
                 index='movies',
                 params=f"{title}{sort}{page_size}{genre}"
             )
-
             assert redis_client.get(key=key) is not None
             await redis_client.flushall()
             assert await redis_client.get(key=key) is None
--- fastapi_solution/tests/functional/testdata/es_schema/persons.py
+++ fastapi_solution/tests/functional/testdata/es_schema/persons.py
@@ -2,15 +2,13 @@ class Persons:
     mappings = {
         "dynamic": "strict",
         "properties": {
-            "id": {
-                "type": "keyword"
-            },
+            "id": {"type": "keyword"},
             "full_name": {
                 "type": "text",
-                "analyzer": "ru_en"
+                "analyzer": "ru_en",
+                "fields": {"raw": {"type": "keyword"}},
             },
-            "films_ids": {
-                "type": "keyword"
-            }
+            "films_ids": {"type": "keyword"},
+
         }
     }
