1bb1bca92e8265b0fe5dfeb04d43863bac9075a7
user <79fz270704@gmail.com>
2022-04-18 18:15:19 +0300

    #50 db schema changes, role.title now primary key

--- auth_db/initdb.d/auth_db.sql
+++ auth_db/initdb.d/auth_db.sql
@@ -3,8 +3,7 @@
 --
 CREATE TABLE "roles"
 (
-    "id"    uuid        NOT NULL PRIMARY KEY,
-    "title" varchar(50) NOT NULL UNIQUE
+    "title" varchar(50) NOT NULL PRIMARY KEY
 );
 --
 -- Create model User
@@ -23,11 +22,11 @@ CREATE TABLE "users"
 --
 -- Create model UserRole
 --
-CREATE TABLE "users_role"
+CREATE TABLE "users_roles"
 (
-    "id"      uuid NOT NULL PRIMARY KEY,
-    "role_id" uuid NOT NULL,
-    "user_id" uuid NOT NULL
+    "id"      uuid        NOT NULL PRIMARY KEY,
+    "role_id" varchar(50) NOT NULL,
+    "user_id" uuid        NOT NULL
 );
 --
 -- Add field roles to user
@@ -42,19 +41,18 @@ CREATE TABLE "login_history"
     "created_at" timestamp with time zone NOT NULL,
     "user_id"    uuid                     NOT NULL
 );
-
 CREATE INDEX "roles_title_c2f44585_like" ON "roles" ("title" varchar_pattern_ops);
 CREATE INDEX "users_login_3b007138_like" ON "users" ("login" varchar_pattern_ops);
 CREATE INDEX "users_email_0ea73cca_like" ON "users" ("email" varchar_pattern_ops);
 
-ALTER TABLE "users_role"
-    ADD CONSTRAINT "users_role_role_id_bace185c_fk_roles_id" FOREIGN KEY ("role_id") REFERENCES "roles" ("id") DEFERRABLE INITIALLY DEFERRED;
+ALTER TABLE "users_roles"
+    ADD CONSTRAINT "users_roles_role_id_fe00bf26_fk_roles_title" FOREIGN KEY ("role_id") REFERENCES "roles" ("title") DEFERRABLE INITIALLY DEFERRED;
+ALTER TABLE "users_roles"
+    ADD CONSTRAINT "users_roles_user_id_a7500f73_fk_users_id" FOREIGN KEY ("user_id") REFERENCES "users" ("id") DEFERRABLE INITIALLY DEFERRED;
 
-ALTER TABLE "users_role"
-    ADD CONSTRAINT "users_role_user_id_cbcc337e_fk_users_id" FOREIGN KEY ("user_id") REFERENCES "users" ("id") DEFERRABLE INITIALLY DEFERRED;
-
-CREATE INDEX "users_role_role_id_bace185c" ON "users_role" ("role_id");
-CREATE INDEX "users_role_user_id_cbcc337e" ON "users_role" ("user_id");
+CREATE INDEX "users_roles_role_id_fe00bf26" ON "users_roles" ("role_id");
+CREATE INDEX "users_roles_role_id_fe00bf26_like" ON "users_roles" ("role_id" varchar_pattern_ops);
+CREATE INDEX "users_roles_user_id_a7500f73" ON "users_roles" ("user_id");
 
 ALTER TABLE "login_history"
     ADD CONSTRAINT "login_history_user_id_0eeaebb8_fk_users_id" FOREIGN KEY ("user_id") REFERENCES "users" ("id") DEFERRABLE INITIALLY DEFERRED;
