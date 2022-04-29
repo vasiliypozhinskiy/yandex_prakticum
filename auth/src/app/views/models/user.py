c7af72da4ad9cbb9b22ac422d754fafb516c8530
Aleksandr Lemikhov <lemikhovalex@gmail.com>
2022-04-29 10:12:43 +0300

    publish not sensetive information

--- auth/src/app/views/models/user.py
+++ auth/src/app/views/models/user.py
@@ -5,13 +5,9 @@ from pydantic.main import BaseModel
 
 
 class UserResponse(BaseModel):
-    id: UUID
     login: str
-    is_superuser: bool
     email: str
     first_name: Optional[str]
     middle_name: Optional[str]
     last_name: Optional[str]
 
-    history_entries: Optional[List[UUID]]
-    roles: Optional[List[str]]
