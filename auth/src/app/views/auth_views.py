ba7d8d977a4168d28c993fe85d8ca945a111da89
Aleksandr Lemikhov <lemikhovalex@gmail.com>
2022-05-02 07:23:28 +0300

    fix swagger

--- auth/src/app/views/auth_views.py
+++ auth/src/app/views/auth_views.py
@@ -3,7 +3,7 @@ import uuid
 from typing import Optional
 from http import HTTPStatus
 
-from flask.views import View
+from flask.views import MethodView, View
 from flask import Blueprint, jsonify, request
 from flasgger.utils import swag_from
 
@@ -14,56 +14,51 @@ from app.views.utils.decorator import catch_exceptions
 
 auth_blueprint = Blueprint('auth', __name__, url_prefix='/auth/api/v1')
 
-class Login(View):
-    methods = ['POST']
+class Login(MethodView):
     
     @swag_from(f'{SWAGGER_DOCS_PATH}/auth/login.yaml', endpoint='auth.login', methods=['POST'])
     @catch_exceptions
-    def dispatch_request(self):
+    def post(self):
         request_data = request.json
         login_data = AuthReqView.parse_obj(request_data)
         login_resp = AUTH_SERVICE.login(login_data)
         return login_resp.dict()
 
 
-class LogOut(View):
-    methods = ['POST']
+class LogOut(MethodView):
     
     @swag_from(f'{SWAGGER_DOCS_PATH}/auth/logout.yaml', endpoint='auth.logout', methods=['POST'])
     @catch_exceptions
-    def dispatch_request(self):
+    def post(self):
         access_token = request.headers["Authorization"]
         AUTH_SERVICE.logout(access_token=access_token)
         return 'Success logout', HTTPStatus.OK
 
 
-class Authorize(View):
-    methods = ['POST']
+class Authorize(MethodView):
     
     @swag_from(f'{SWAGGER_DOCS_PATH}/auth/authorize.yaml', endpoint='auth.authorize', methods=['POST'])
     @catch_exceptions
-    def dispatch_request(self):
+    def post(self):
         access_token = request.headers["Authorization"]
         roles = AUTH_SERVICE.authorize(access_token=access_token)
         return jsonify(roles), HTTPStatus.OK
 
 
-class Refresh(View):
-    methods = ['POST']
+class Refresh(MethodView):
     
     @swag_from(f'{SWAGGER_DOCS_PATH}/auth/refresh.yaml', endpoint='auth.refresh', methods=['POST'])
     @catch_exceptions
-    def dispatch_request(self):
+    def post(self):
         req = AuthRefreshReqView(**request.json)
         response = AUTH_SERVICE.refresh_jwt(refresh_jwt=req.refresh_token)
         return response.dict()
 
 
-class BaseLogoutAll(View):
-    methods = ['POST']
+class BaseLogoutAll(MethodView):
     
     @catch_exceptions
-    def dispatch_request(self, user_id: Optional[str] = None):
+    def post(self, user_id: Optional[str] = None):
         access_token = request.headers["Authorization"]
         if user_id is not None:
             user_id = uuid.UUID(user_id)
@@ -76,8 +71,8 @@ class LogOutAllById(BaseLogoutAll):
     @swag_from(f'{SWAGGER_DOCS_PATH}/auth/logout_all_id.yaml', endpoint='auth.logout-all-id', methods=['POST'])
     @catch_exceptions
     @AUTH_SERVICE.token_required(check_is_superuser=True)
-    def dispatch_request(self, user_id: Optional[str] = None):
-        return super().dispatch_request(user_id=user_id)
+    def post(self, user_id: Optional[str] = None):
+        return super().post(user_id=user_id)
 
 
 
@@ -86,13 +81,13 @@ class LogOutAllByAccess(BaseLogoutAll):
     @swag_from(f'{SWAGGER_DOCS_PATH}/auth/logout_all.yaml', endpoint='auth.logout-all-access', methods=['POST'])
     @catch_exceptions
     @AUTH_SERVICE.token_required()
-    def dispatch_request(self, user_id: Optional[str] = None):
-        return super().dispatch_request(user_id=user_id)
+    def post(self, user_id: Optional[str] = None):
+        return super().post(user_id=user_id)
 
 
-auth_blueprint.add_url_rule('/login/', endpoint='login', view_func=Login.as_view('auth'))
-auth_blueprint.add_url_rule('/logout', endpoint='logout', view_func=LogOut.as_view('auth'))
-auth_blueprint.add_url_rule('/authorize/', endpoint='authorize', view_func=Authorize.as_view('auth'))
-auth_blueprint.add_url_rule('/refresh/', endpoint='refresh', view_func=Refresh.as_view('auth'))
-auth_blueprint.add_url_rule('/logout_all/<string:user_id>', endpoint='logout-all-id', view_func=LogOutAllById.as_view('auth'))
-auth_blueprint.add_url_rule('/logout_all/', endpoint='logout-all-access', view_func=LogOutAllByAccess.as_view('auth'))
+auth_blueprint.add_url_rule('/login/', endpoint='login', view_func=Login.as_view('auth'), methods=["POST"])
+auth_blueprint.add_url_rule('/logout', endpoint='logout', view_func=LogOut.as_view('auth'), methods=["POST"])
+auth_blueprint.add_url_rule('/authorize/', endpoint='authorize', view_func=Authorize.as_view('auth'), methods=["POST"])
+auth_blueprint.add_url_rule('/refresh/', endpoint='refresh', view_func=Refresh.as_view('auth'), methods=["POST"])
+auth_blueprint.add_url_rule('/logout_all/<string:user_id>', endpoint='logout-all-id', view_func=LogOutAllById.as_view('auth'), methods=["POST"])
+auth_blueprint.add_url_rule('/logout_all/', endpoint='logout-all-access', view_func=LogOutAllByAccess.as_view('auth'), methods=["POST"])
