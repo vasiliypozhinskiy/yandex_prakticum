7e80f0a8d0907deec827a693bbe81d9344d1d91c
Vlad <Promotion-kmv@yandex.ru>
2022-05-02 13:59:12 +0300

    migrate database

--- auth/src/app/core/__init__.py
+++ auth/src/app/core/__init__.py
@@ -3,6 +3,7 @@ from app.core.swagger_config import swagger_config, swagger_template
 from flasgger import Swagger
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__, instance_relative_config=True)
 
@@ -11,3 +12,5 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 
 swagger = Swagger(app, config=swagger_config, template=swagger_template)
+
+migrate = Migrate(app, db)
--- auth/src/main.py
+++ auth/src/main.py
@@ -1,19 +1,24 @@
 import os
 
-from app.core import app
+from app.core import app, db, migrate
 from app.views.user_view import user_blueprint
 from app.views.role_view import role_blueprint
 from app.views.user_add_role import user_role_blueprint
 from app.views.auth_views import auth_blueprint
 
-app.register_blueprint(role_blueprint)
-app.register_blueprint(user_role_blueprint)
-app.register_blueprint(user_blueprint)
-app.register_blueprint(auth_blueprint)
 
-
-if __name__ == "__main__":
-    app.run(
+def create_app(flask_app):
+    db.init_app(app=flask_app)
+    app.register_blueprint(role_blueprint)
+    app.register_blueprint(user_role_blueprint)
+    app.register_blueprint(user_blueprint)
+    app.register_blueprint(auth_blueprint)
+    migrate.init_app(app, db)
+    flask_app.run(
         host=os.getenv('HOST', 'localhost'),
         debug=bool(os.getenv('DEBUG', 1)),
     )
+
+
+if __name__ == "__main__":
+    create_app(flask_app=app)
