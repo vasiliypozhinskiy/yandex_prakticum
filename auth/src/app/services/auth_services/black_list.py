7bb41b896d6e0e8b59a4bbed938890eb6dd0eae7
Aleksandr Lemikhov <lemikhovalex@gmail.com>
2022-05-07 15:05:18 +0300

    add tracing to db postgre call and redis

--- auth/src/app/services/auth_services/black_list.py
+++ auth/src/app/services/auth_services/black_list.py
@@ -8,6 +8,10 @@ from app.db.redis import redis_revoked_tokens, redis_log_out_all, redis_upd_role
 from app.core.config import REFRESH_TOKEN_EXP, ACCESS_TOKEN_EXP
 from app.services.auth_services.jwt_service import JWT_SERVICE
 
+from opentelemetry import trace
+
+tracer = trace.get_tracer(__name__)
+
 
 class BaseBlackList(ABC):
     @abstractmethod
@@ -26,15 +30,17 @@ class TokenBlackList(BaseBlackList):
         self.exp_time = exp_time
 
     def add(self, token: str) -> None:
-        if token != "":
-            self.storage.setex(
-                token,
-                self.exp_time,
-                self.reason
-            )
+        with tracer.start_as_current_span('TokenBlackList-add'):
+            if token != "":
+                self.storage.setex(
+                    token,
+                    self.exp_time,
+                    self.reason
+                )
 
     def is_ok(self, token: str) -> bool:
-        return not bool(self.storage.exists(token))
+        with tracer.start_as_current_span('TokenBlackList-is_ok'):
+            return not bool(self.storage.exists(token))
 
 
 class UserIDBlackList(BaseBlackList):
@@ -43,21 +49,24 @@ class UserIDBlackList(BaseBlackList):
         self.exp_time = exp_time
 
     def add(self, user_id: uuid.UUID) -> None:
-        self.storage.setex(
-            str(user_id),
-            REFRESH_TOKEN_EXP,
-            get_now_ms(),
-        )
+        
+        with tracer.start_as_current_span('UserIDBlackList-add'):
+            self.storage.setex(
+                str(user_id),
+                REFRESH_TOKEN_EXP,
+                get_now_ms(),
+            )
 
     def is_ok(self, access_token: str) -> bool:
-        payload = JWT_SERVICE.get_access_payload(access_token)
-        set_time = self.storage.get(str(payload.user_id))
-        if set_time is None:
-            return True  # no request to logout for this user
-        set_time = int(set_time.decode())
-        if payload.iat < set_time:
-            return False  # logged in after request on logout
-        return True
+        with tracer.start_as_current_span('UserIDBlackList-is_ok'):
+            payload = JWT_SERVICE.get_access_payload(access_token)
+            set_time = self.storage.get(str(payload.user_id))
+            if set_time is None:
+                return True  # no request to logout for this user
+            set_time = int(set_time.decode())
+            if payload.iat < set_time:
+                return False  # logged in after request on logout
+            return True
 
 
 REVOKED_ACCESS = TokenBlackList(storage=redis_revoked_tokens, exp_time=ACCESS_TOKEN_EXP, reason='revoked')
